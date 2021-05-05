8df152ec3a317a52b431193e73e04bc005857633
Hasan Ozovali <ozovalihasan@gmail.com>
2021-05-05 12:49:21 +0300

    Update parameter name

--- lib/association.rb
+++ lib/association.rb
@@ -1,12 +1,12 @@
 class Association
   attr_accessor :first_item, :second_items, :name
 
-  def initialize(first_item, item)
+  def initialize(first_item, association_block)
     @first_item = first_item
     @second_items = []
-    @name = item.content
-    if item.sub_blocks
-      item.sub_blocks.map do |sub_block|
+    @name = association_block.content
+    if association_block.sub_blocks
+      association_block.sub_blocks.map do |sub_block|
         @second_items << Item.new(sub_block, { item: first_item, association: self }, first_item.parent[:item])
       end
     end
@@ -15,5 +15,5 @@ class Association
   def self.all
     ObjectSpace.each_object(self).to_a
   end
-  
+
 end

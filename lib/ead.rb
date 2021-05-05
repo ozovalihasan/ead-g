c50c9abf4964a025efc4ab8b417e9b7cb76dd66f
Hasan Ozovali <ozovalihasan@gmail.com>
2021-05-06 01:08:57 +0300

    Update ead file as class

--- lib/ead.rb
+++ lib/ead.rb
@@ -1,33 +1,32 @@
-require 'rails'
 require 'json'
 require 'fileutils'
-require 'association'
-require 'attribute'
 require 'item'
 require 'block'
 require 'byebug'
 
-def import_JSON(user_arguments)
-  file = File.read(user_arguments[0] || './EAD.json')
-  items = JSON.parse(file)
-  ead_id = '8'
-  Block.new(ead_id, items)
-end
-
-def check_implement_items
-  ead_id = '8'
-  block = Block.find(ead_id)
-  block.sub_blocks.map do |sub_block|
-    Item.new(sub_block)
+class EAD
+  def import_JSON(user_arguments)
+    file = File.read(user_arguments[0] || './EAD.json')
+    items = JSON.parse(file)
+    ead_id = '8'
+    Block.new(ead_id, items)
   end
 
-  Item.all.reverse.each do |item|
-    item.create_migration
-    item.add_associations_to_model
-  end
-end
+  def check_implement_items
+    ead_id = '8'
+    block = Block.find(ead_id)
+    block.sub_blocks.map do |sub_block|
+      Item.new(sub_block)
+    end
 
-def start(user_arguments)
-  import_JSON(user_arguments)
-  check_implement_items
+    Item.all.reverse.each do |item|
+      item.create_migration
+      item.add_associations_to_model
+    end
+  end
+
+  def start(user_arguments)
+    import_JSON(user_arguments)
+    check_implement_items
+  end
 end

95c8736cafbd6bf5f79cf7f42e287d249d33fc60
Hasan Ozovali <ozovalihasan@gmail.com>
2021-05-20 14:43:40 +0300

    Update tests of Block

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -4,39 +4,46 @@ describe Block do
   before do
     ObjectSpace.garbage_collect
     items = {
-      '9' => {
+      '10' => {
         'content' => 'entity1',
         'subItemIds' => [
-          10
+          11
         ],
-        'category' => 'entity'
+        'category' => 'entity',
+        'entity' => true
       },
-      '10' => {
+      '11' => {
         'content' => 'association1',
         'subItemIds' => [
-          11
+          12
         ],
         'association' => true,
         'category' => 'association'
       },
-      '11' => {
+      '12' => {
         'content' => 'entity2',
         'subItemIds' => [],
         'category' => 'entity'
       }
     }
-    @block = Block.new('9', items)
+    @block = Block.new('10', items)
   end
 
   describe '#initialize' do
     it 'creates an instance of the class correctly' do
-      expect(@block.id).to eq('9')
+      expect(@block.id).to eq('10')
       expect(@block.content).to eq('entity1')
       expect(@block.category).to eq('entity')
       expect(@block.attribute_container).to eq(nil)
       expect(@block.attribute).to eq(nil)
+      expect(@block.entity).to eq(true)
+      expect(@block.entity_container).to eq(nil)
       expect(@block.type).to eq(nil)
       expect(@block.association).to eq(nil)
+      expect(@block.entity_clone).to eq(nil)
+      expect(@block.cloneable).to eq(nil)
+      expect(@block.clone_blocks).to eq(nil)
+      expect(@block.clone_parent).to eq('')
       expect(@block.sub_blocks.size).to eq(1)
     end
   end
@@ -49,7 +56,7 @@ describe Block do
 
   describe '.find' do
     it 'finds a block with its id' do
-      expect(Block.find('9').content).to eq('entity1')
+      expect(Block.find('10').content).to eq('entity1')
     end
   end
 end

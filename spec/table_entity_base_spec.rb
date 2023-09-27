114544f881befd84cbea7772f533f693c549aeaf
ozovalihasan <ozovalihasan@gmail.com>
2023-09-28 01:19:27 +0300

    Fix a test of TableEntityBase

--- spec/table_entity_base_spec.rb
+++ spec/table_entity_base_spec.rb
@@ -21,7 +21,7 @@ describe TableEntityBase do
 
   describe '.all' do
     it 'returns all created instances' do
-      expect(TableEntityBase.all.size).to eq(51)
+      expect(TableEntityBase.all.size).to eq(58)
     end
   end
 
